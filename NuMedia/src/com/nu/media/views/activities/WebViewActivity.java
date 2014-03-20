dd028d082252a702f8e606000bd0feb4f9e19d3b
dazziest <dazziest@gmail.com>
2014-03-20 12:02:39 +0800

    add quiz feature

--- NuMedia/src/com/nu/media/views/activities/WebViewActivity.java
+++ NuMedia/src/com/nu/media/views/activities/WebViewActivity.java
@@ -4,7 +4,9 @@ import com.nu.media.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebSettings.RenderPriority;
 
 public class WebViewActivity extends Activity {
 
@@ -19,6 +21,8 @@ public class WebViewActivity extends Activity {
  
 	   String customHtml = getIntent().getExtras().getString("web_source");
 	   webView.loadData(customHtml, "text/html", "UTF-8");
+	   webView.getSettings().setRenderPriority(RenderPriority.HIGH);
+	   webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
 
 		overridePendingTransition(R.anim.activity_open_translate,R.anim.activity_close_scale);
 	}
--- NuMedia/src/com/nu/media/views/fragments/MainQuizFragment.java
+++ NuMedia/src/com/nu/media/views/fragments/MainQuizFragment.java
@@ -1,10 +1,41 @@
 package com.nu.media.views.fragments;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.RenderPriority;
+import android.webkit.WebView;
+
+import com.nu.media.R;
 import com.nu.media.models.Article;
 import com.nu.media.models.dao.DataAccess;
 
 public class MainQuizFragment extends BaseContentFragment {
 
+	private WebView webView;
+
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		setActionBarTitle("Favorite");
+		
+		View v = inflater.inflate(R.layout.activity_webview, container, false);
+		
+		webView = (WebView) v.findViewById(R.id.webview);
+		webView.getSettings().setJavaScriptEnabled(true);
+//		String customHtml = getActivity().getIntent().getExtras().getString("web_source");
+//	 	webView.loadData("file:///android_asset/www/jQuizzy.htm", "text/html", "UTF-8");
+	 	webView.loadUrl("file:///android_asset/www/jQuizzy.htm");
+	 	webView.getSettings().setRenderPriority(RenderPriority.HIGH);
+	 	webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+//	 	webView.loadUrl("http://demos.jquerymobile.com/1.2.0/docs/pages/page-transitions.html");
+	 	
+	 	return v;
+	}
+	
 	@Override
 	public void retrieveData() {
 		// TODO Auto-generated method stub
